9c32752de4d5d436571eb01796c6c439ea1f2ed6
lnwu <wind2729@gmail.com>
2026-02-14 23:25:13 +0800

    [main]: Refactor AppDelegate and add LanguageOptimizerApp structure; update project settings and documentation

---
+++ Sources/LanguageOptimizer/LanguageOptimizerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct LanguageOptimizerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    var body: some Scene {
+        Settings {
+            SettingsView()
+        }
+    }
+}
